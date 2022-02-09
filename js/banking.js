ccf6b2a27b4d6f2acf6a68e86b9fbc3e4c34ec38
Kamrul Hasan <kamrulsdev@gmail.com>
2022-02-09 23:18:13 +0600

    user deposite and total value added with js event handler

---
+++ js/banking.js
@@ -0,0 +1,14 @@
+// handle deposit button event
+document.getElementById('deposit-btn').addEventListener('click', function(){   
+    // get amount deposited
+    const depositInput = document.getElementById('deposit-input');
+    const depositAmount = depositInput.value;
+
+    // show the value in Deposit section
+    const depositTotal = document.getElementById('deposit-total');
+    depositTotal.innerText = depositAmount;
+
+    
+    
+    
+});
