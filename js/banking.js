da19dcb3c5851b95e0465fb6af82fc7d7a1a5926
Kamrul Hasan <kamrulsdev@gmail.com>
2022-02-09 23:42:20 +0600

    Deposit field is completed

--- js/banking.js
+++ js/banking.js
@@ -2,11 +2,15 @@
 document.getElementById('deposit-btn').addEventListener('click', function(){   
     // get amount deposited
     const depositInput = document.getElementById('deposit-input');
-    const depositAmount = depositInput.value;
+    const newDepositAmount = depositInput.value;
 
     // show the value in Deposit section
     const depositTotal = document.getElementById('deposit-total');
-    depositTotal.innerText = depositAmount;
+
+    const previousDepositAmount = depositTotal.innerText;
+    const newDepositTotal = parseFloat(previousDepositAmount) + parseFloat(newDepositAmount);
+
+    depositTotal.innerText = newDepositTotal;
 
     // clear the deposit input field
     depositInput.value = '';
