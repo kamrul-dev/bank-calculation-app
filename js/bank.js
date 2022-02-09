71a0e1faac1a04e2fe19d9a248b44424b52644eb
Kamrul Hasan <kamrulsdev@gmail.com>
2022-02-09 21:40:25 +0600

    user email and pass put in variables using event handler in login button

---
+++ js/bank.js
@@ -0,0 +1,13 @@
+
+document.getElementById('login-btn').addEventListener('click', function(event){
+    //  get user email
+    const emailField = document.getElementById('user-email');
+    const userEmail = emailField.value;
+
+    // get user password
+    const passwordField = document.getElementById('user-password');
+    const userPassword = passwordField.value;
+    
+    
+    
+});
